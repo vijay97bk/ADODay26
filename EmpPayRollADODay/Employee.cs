41f9d5a974cb18960147d11dad9592e1ad2c734b
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-14 22:00:36 +0530

    [vijay]RetrivingDataFromDB

---
+++ EmpPayRollADODay/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpPayRollADODay
+{
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public DateTime startDate;
+        public char gender;
+        public long phoneNumber;
+        public string address;
+        public string[] department;
+        public int basicPay;
+        public int deduction;
+        public int taxablePay;
+        public int incomeTax;
+        public int netPay;
+        public string companyName;
+
+        public Employee()
+        {
+            department = new string[10];
+        }
+    }
+}
--- EmpPayRollADODay/Program.cs
+++ EmpPayRollADODay/Program.cs
@@ -6,7 +6,37 @@ namespace EmpPayRollADODay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EmpPayroll empPayroll = new EmpPayroll();
+            int option;
+            do
+            {
+                Console.WriteLine("1)Insert Record");
+                Console.WriteLine("2)Read All records");
+                Console.WriteLine("3)Update Record");
+                Console.WriteLine("4)Delete Record");
+                Console.WriteLine("5)Exit");
+                Console.WriteLine("Enter Choice");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        empPayroll.InsertRecordStoredProcedure();
+                        break;
+                    case 2:
+                        empPayroll.ReturnAllRecords();
+                        break;
+                    case 3:
+                        empPayroll.ReturnAllRecords();
+                        empPayroll.UpdateRecord();
+                        break;
+                    case 4:
+                        empPayroll.ReturnAllRecords();
+                        empPayroll.DeleteRecordStoredProcedure();
+                        break;
+                    default:
+                        break;
+                }
+            } while (option != 5);
         }
     }
 }
