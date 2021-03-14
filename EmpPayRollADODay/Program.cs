7c2f87faddf62bcb436d546655fca2520552a72e
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-14 21:34:40 +0530

    [vijay]FirstCommit

---
+++ EmpPayRollADODay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpPayRollADODay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
