2847ddb648031998a4772b31ba2ab71dae11334c
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-16 23:04:28 +0530

    [vijay]SelectedDataFromRangeofDate

--- EmpPayRollADODay/EmployeeRepo.cs
+++ EmpPayRollADODay/EmployeeRepo.cs
@@ -15,10 +15,13 @@ namespace EmpPayRollADODay26
         public static string ConnectionString = "Server=DESKTOP-ERQSHRG;Initial Catalog=Payroll_Service;Integrated Security=SSPI;";
         SqlConnection connection = new SqlConnection(ConnectionString);
         List<Employee> employees = new List<Employee>();
-
+        /// <summary>
+        /// Gets all employee. UC2
+        /// </summary>
+        /// <returns></returns>
         public List<Employee> GetAllEmployee()
         {
-            
+
             Employee employeeModel = new Employee();
             using (connection)
             {
@@ -46,7 +49,7 @@ namespace EmpPayRollADODay26
                         employeeModel.net_pay = dr.GetInt32(5);
                         employeeModel.company = dr.GetString(5);
                         employees.Add(employeeModel);
-                        Console.WriteLine(employeeModel.id+","+employeeModel.name+","+employeeModel.start);
+                        Console.WriteLine(employeeModel.id + "," + employeeModel.name + "," + employeeModel.start);
                     }
                 }
                 connection.Close();
@@ -59,7 +62,7 @@ namespace EmpPayRollADODay26
             SqlConnection connection = new SqlConnection(ConnectionString);
             using (connection)
             {
-                SqlCommand command = new SqlCommand("SpAddEmployeeDetails", connection) ;
+                SqlCommand command = new SqlCommand("SpAddEmployeeDetails", connection);
                 command.CommandType = CommandType.StoredProcedure;
                 command.Parameters.AddWithValue("@id", employee.id);
                 command.Parameters.AddWithValue("@name", employee.name);
@@ -73,15 +76,18 @@ namespace EmpPayRollADODay26
                 command.Parameters.AddWithValue("@income_tax", employee.income_tax);
                 command.Parameters.AddWithValue("@net_pay", employee.net_pay);
                 connection.Open();
-                var result=command.ExecuteNonQuery();
+                var result = command.ExecuteNonQuery();
                 connection.Close();
                 if (result != 0)
                 {
                     return true;
                 }
-                    return false;
+                return false;
             }
         }
+        /// <summary>
+        /// Updates the record. UC3
+        /// </summary>
         public void UpdateRecord()
         {
             string query = @"update employee_payroll set basic_pay=3000000 where name='Terisa';";
@@ -91,6 +97,21 @@ namespace EmpPayRollADODay26
             cmd.ExecuteNonQuery();
             connection.Close();
         }
+        /// <summary>
+        /// Retrives the data from range. UC5
+        /// </summary>
+        /// <param name="range1">The range1.</param>
+        /// <param name="range2">The range2.</param>
+        public void RetriveDataFromRange()
+        {
+            string query = @"select * from employee_payroll where start between '2002-01-01' and  GETDATE();";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+        
 
     }
 }
